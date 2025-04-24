4d16247a7bf6fcbfb51b94ea0a37e63ed0678150
nrthompson925 <nrthompson@bluevalleyk12.net>
2025-04-24 13:00:04 -0500

    update 4/24/25

--- Chapter_7/pp2/main.cpp
+++ Chapter_7/pp2/main.cpp
@@ -29,10 +29,10 @@ void convertCharToInt (char charArr[], int intArr[], int size=10) {
         else if (charArr[index] == 'd') intArr[index] =13;
         else if (charArr[index] == 'e') intArr[index] =14;
         else if (charArr[index] == 'f') intArr[index] =15;
-        else {
-            cout<<"\nInvalid Character when converting character to integer";
-            exit(1);
-        };
+        //else {
+            //cout<<"\nInvalid Character when converting character to integer";
+            //exit(1);
+        //};
     };
 };
 
@@ -51,32 +51,35 @@ bool sumArrays(int intArr1[], int intArr2[], int sum[],
     return false;
 };
 
-void convertIntToHex(int intArr[], char charArr[], int size=10) {
+int convertIntToHex(int sum[], char charArr[], int size=10) {
     for (int index = 0; index <size;++index) {
-        if (intArr[index] ==0) charArr[index] = '0';
-        else if (intArr[index] == '1') charArr[index] = '1';
-        else if (intArr[index] == '2') charArr[index] = '2';
-        else if (intArr[index] == '3') charArr[index] = '3';
-        else if (intArr[index] == '4') charArr[index] = '4';
-        else if (intArr[index] == '5') charArr[index] = '5';
-        else if (intArr[index] == '6') charArr[index] = '6';
-        else if (intArr[index] == '7') charArr[index] = '7';
-        else if (intArr[index] == '8') charArr[index] = '8';
-        else if (intArr[index] == '9') charArr[index] = '9';
-        else if (intArr[index] == 'a') charArr[index] = 'a';
-        else if (intArr[index] == 'b') charArr[index] = 'b';
-        else if (intArr[index] == 'c') charArr[index] = 'c';
-        else if (intArr[index] == 'd') charArr[index] = 'd';
-        else if (intArr[index] == 'e') charArr[index] = 'e';
-        else if (intArr[index] == 'f') charArr[index] = 'f';
-        else {
-            cout<<"\nInvalid Character when converting integer to hex";
-            exit(1);
-        };
+        if (sum[index] ==0) charArr[index] = '0';
+        else if (sum[index] == '1') charArr[index] = '1';
+        else if (sum[index] == '2') charArr[index] = '2';
+        else if (sum[index] == '3') charArr[index] = '3';
+        else if (sum[index] == '4') charArr[index] = '4';
+        else if (sum[index] == '5') charArr[index] = '5';
+        else if (sum[index] == '6') charArr[index] = '6';
+        else if (sum[index] == '7') charArr[index] = '7';
+        else if (sum[index] == '8') charArr[index] = '8';
+        else if (sum[index] == '9') charArr[index] = '9';
+        else if (sum[index] == '10') charArr[index] = 'a';
+        else if (sum[index] == '11') charArr[index] = 'b';
+        else if (sum[index] == '12') charArr[index] = 'c';
+        else if (sum[index] == '13') charArr[index] = 'd';
+        else if (sum[index] == '14') charArr[index] = 'e';
+        else if (sum[index] == '15') charArr[index] = 'f';
+
+        return sum[0], sum[1];
+
+        //else {
+            //cout<<"\nInvalid Character when converting integer to hex";
+            //exit(1);
+        //};
     };
 };
 
-int main() {
+int main(int sum) {
     char char1[10]={'0'};
     char char2[10]={'0'};
     int int1[10]={0};
@@ -97,16 +100,26 @@ int main() {
         convertCharToInt(char1, int1);
         convertCharToInt(char2, int2);
 
+        int size= 10) {
+            int mind = 0;
+
+            for (int index = size-1; index >=0;--index) {
+                sum[index] = intArr1[index] +intArr2[index] + mind;
+
+                mind = sum[index]/16;
+                sum[index] = sum[index]%16;
+            };
+
         if (sumArrays(int1, int2, intResult)) {
 
             convertIntToHex(intResult, charResult);
-            cout<<"\nThe addition result is ";
-            for (char element : charResult)cout<<element;
+            cout<<"\nThe addition result is "<<sum[1]<<sum[2]<<sum[3];
         }else
             cout<<"\nAddition overflow";
 
         cout<<"\nEnter 1 to repeat or 0 to quit";
+        cout<<convertIntToHex(intResult, charResult, 10);
         cin>>repeat;
     }while (repeat);
     return 0;
-}
+};
